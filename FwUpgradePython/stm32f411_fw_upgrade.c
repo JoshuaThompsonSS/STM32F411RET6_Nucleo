9d8754249cbdcb25f6f4ab1a6d11e17a28e1043e
JoshuaThompsonSS <joshuathompson@sonicsensory.com>
2016-07-14 19:13:29 -0700

    fixed bugs in c stmfw upgrade code

--- FwUpgradePython/stm32f411_fw_upgrade.c
+++ FwUpgradePython/stm32f411_fw_upgrade.c
@@ -1,6 +1,9 @@
-/* Verbose level */
-QUIET = 20
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+/* Verbose level */
+#define QUIET                   20
 /* these come from AN2606 */
 #define CHIP_ID_MAX_STR         100
         
@@ -13,17 +16,17 @@ typedef struct error_status_t{
 	int unknown;
 }error_status_t;
 
-typedef enum error_status_type_t {NACK_ERROR, TIMEOUT_ERROR; DATA_LEN_ERROR; UNKNOWN_ERROR}error_status_type_t;
+typedef enum error_status_type_t {NACK_ERROR=0, TIMEOUT_ERROR=1, DATA_LEN_ERROR=2, UNKNOWN_ERROR=3} error_status_type_t;
 
 
-error_status_t errorStatus = {0; 0; 0; 0};
+error_status_t errorStatus = {0, 0, 0, 0};
 
 typedef struct chip_id_t{
     unsigned int id;
     unsigned char name[CHIP_ID_MAX_STR];
 } chip_id_t;
 
-typdef struct chip_ids_t {
+typedef struct chip_ids_t {
     chip_id_t lowDensity;
     chip_id_t medDensity;
     chip_id_t highDensity;
@@ -38,6 +41,7 @@ typdef struct chip_ids_t {
 /* TODO: add string names to name attribute */
 /* This struct isn't used at the moment but it can be used to verify the chip we are communicating with is correct */
 chip_ids_t chipIDs;
+/*
 chipIDs.lowDensity.id = 0x412;
 chipIDs.medDensity.id = 0x410;
 chipIDs.highDensity.id = 0x414;
@@ -47,9 +51,10 @@ chipIDs.xlDensity.id = 0x430;
 chipIDs.medDensityUlPwrLine.id = 0x416;
 chipIDs.stm32F2xx.id = 0x411;
 chipIDs.stm32F4xx.id = 0x413;
+ */
 
 /* Handle error */
-void handle_error(error_status_type_t * error_type){
+void handle_error(error_status_type_t error_type){
 	switch(error_type){
 		case NACK_ERROR:
 			errorStatus.nack = 1;
@@ -91,8 +96,9 @@ char uart_read(void){
     return c;
 }
 
-char uart_reads(unsigned char* buffer, int len){
+void uart_reads(unsigned char* buffer, int len){
     /* TODO: use real uart read multiple char function when ready */
+    
 }
 
 /* dummy uart write function */
@@ -138,13 +144,14 @@ void open(void){
 }
 
 /* Wait for nack or ack response */
-void wait_for_ask(void){
+int wait_for_ask(void){
     /* wait for ask */
     char ask = uart_read();
     /* TODO: what to do if timeout? */
     if(uart_timeout()){
         /* Timeout handler here */
 		handle_error(TIMEOUT_ERROR);
+        return 0;
     }
     else{
         if(ask == 0x79){
@@ -162,8 +169,11 @@ void wait_for_ask(void){
                 /* Unknown response */
                 /* TODO: how are we going to handle unknown response */
 				handle_error(UNKNOWN_ERROR);
+                return 0;
             }
         }
+    }
+    return 0; /* should not get here */
 }
 
 void reset(void){
@@ -178,7 +188,7 @@ int initChip(void){
     uart_set_rts(0);
     reset();
 	unsigned char cmd[] = {0x7F};
-    uart_write(cmd, 1)       /* tell bootloader to startup */
+    uart_write(cmd, 1);       /* tell bootloader to startup */
     return wait_for_ask();
 }
 
@@ -225,7 +235,7 @@ char cmdGet(void){
 /* Get Version of the ST Device */
 unsigned char cmdGetVersion(void){
     if(cmdGeneric(0x01)){
-        mdebug(10, "*** GetVersion command")
+        mdebug(10, "*** GetVersion command");
         unsigned char version = uart_read();
         unsigned char * buff = (unsigned char *) malloc(sizeof(unsigned char) * 2);
         uart_reads(buff, 2);
@@ -243,7 +253,7 @@ unsigned char cmdGetVersion(void){
 /* Get ID of ST Device */
 int cmdGetID(void){
     if(cmdGeneric(0x02)){
-        mdebug(10, "*** GetID command")
+        mdebug(10, "*** GetID command");
         char len = uart_read();
         unsigned char * buff = (unsigned char *) malloc(sizeof(unsigned char) * (len+1));
         uart_reads(buff, len+1);
@@ -272,10 +282,10 @@ void encode_addr(unsigned long addr, unsigned char * addr_buffer){
 }
 
 
-void cmdReadMemory(unsigned long addr, int lng, unsigned char * data):
+void cmdReadMemory(unsigned long addr, int lng, unsigned char * data){
     if(!(lng <= 256)){
 		handle_error(DATA_LEN_ERROR);
-		return 0;
+        return;
 	} /* TODO: handler assert error */
 
     if(cmdGeneric(0x11)){
@@ -300,9 +310,9 @@ void cmdReadMemory(unsigned long addr, int lng, unsigned char * data):
     }
 }
 
-void cmdGo(unsigned long addr):
-    if self.cmdGeneric(0x21){
-        mdebug(10, "*** Go command")
+void cmdGo(unsigned long addr){
+    if(cmdGeneric(0x21)){
+        mdebug(10, "*** Go command");
         unsigned char * addr_buffer = (unsigned char *) malloc(sizeof(unsigned char)*5);
         encode_addr(addr, addr_buffer); /* Get addr and crc buffer */
         uart_write(addr_buffer, 5);
@@ -312,24 +322,25 @@ void cmdGo(unsigned long addr):
         /* TODO: handle nack */
 		handle_error(NACK_ERROR);
     }
+}
 
 void cmdWriteMemory(unsigned long addr, unsigned char *data){
     if(!(strlen(data) <= 256)){ 
 		handle_error(DATA_LEN_ERROR);
-		return 0;
+        return;
 	}
 	
     if(cmdGeneric(0x31)){
-        mdebug(10, "*** Write memory command")
+        mdebug(10, "*** Write memory command");
         unsigned char * addr_buffer = (unsigned char *) malloc(sizeof(unsigned char)*5);
         encode_addr(addr, addr_buffer); /* Get addr and crc buffer */
         uart_write(addr_buffer, 5);
         wait_for_ask();
-        int lng = (strlen(data)-1) & 0xFF
+        int lng = (strlen(data)-1) & 0xFF;
         mdebug(10, "    %s bytes to write"); /* TODO: debug ? */
         unsigned char length_data[1];
         length_data[0] = lng;
-        uart_write(length_data, 1) /* len really */
+        uart_write(length_data, 1); /* len really */
         unsigned char crc[] = {0xFF};
         unsigned char c[1];
         for(int i = 0; i <= lng; i++){
@@ -348,9 +359,34 @@ void cmdWriteMemory(unsigned long addr, unsigned char *data){
     }
 }
 
-void cmdEraseMemory(unsigned char * sectors = NULL){
+void cmdExtendedEraseMemory(void){
+    if(cmdGeneric(0x44)){
+        mdebug(10, "*** Extended Erase memory command");
+        /* Global mass erase */
+        unsigned char cmd[] = {0xFF};
+        uart_write(cmd, 1);
+        uart_write(cmd, 1);
+        /* Checksum */
+        cmd[0] = 0x00;
+        uart_write(cmd, 1);
+        int tmp = get_uart_timeout();
+        set_uart_timeout(30);
+        /* Extended erase (0x44), can take ten seconds or more */
+        wait_for_ask();
+        set_uart_timeout(tmp); /* set back to default */
+        mdebug(10, "    Extended Erase memory done");
+    }
+    else{
+        /* TODO: handle nack */
+        handle_error(NACK_ERROR);
+    }
+}
+
+
+void cmdEraseMemory(unsigned char * sectors){
     if(extendedErase){
-        return cmdExtendedEraseMemory();
+        cmdExtendedEraseMemory();
+        return;
 	}
 
     if(cmdGeneric(0x43)){
@@ -385,29 +421,9 @@ void cmdEraseMemory(unsigned char * sectors = NULL){
 	}
 }
 
-void cmdExtendedEraseMemory(void){
-    if(cmdGeneric(0x44)){
-        mdebug(10, "*** Extended Erase memory command");
-        /* Global mass erase */
-		unsigned char cmd[] = {0xFF};
-        uart_write(cmd, 1);
-        uart_write(cmd, 1);
-        /* Checksum */
-		cmd[0] = 0x00;
-        uart_write(cmd, 1);
-        int tmp = get_uart_timeout();
-        set_uart_timeout(30);
-        /* Extended erase (0x44), can take ten seconds or more */
-        wait_for_ask();
-        set_uart_timeout(tmp); /* set back to default */
-        mdebug(10, "    Extended Erase memory done");
-    }
-    else{
-        /* TODO: handle nack */
-		handle_error(NACK_ERROR);
-    }
-}
-void cmdWriteProtect(unsigned char * sectors = NULL):
+
+
+void cmdWriteProtect(unsigned char * sectors){
     if(cmdGeneric(0x63)){
         mdebug(10, "*** Write protect command");
 		unsigned char cmd[1];
@@ -430,6 +446,7 @@ void cmdWriteProtect(unsigned char * sectors = NULL):
 		/* TODO: handle nack */
 		handle_error(NACK_ERROR);
     }
+}
 
 void cmdWriteUnprotect(void){
     if(cmdGeneric(0x73)){
@@ -457,9 +474,9 @@ void cmdReadoutProtect(void){
     }
 }
 
-void cmdReadoutUnprotect(void)
+void cmdReadoutUnprotect(void){
     if(cmdGeneric(0x92)){
-        mdebug(10, "*** Readout Unprotect command")
+        mdebug(10, "*** Readout Unprotect command");
         wait_for_ask();
         wait_for_ask();
         mdebug(10, "    Read Unprotect done");
